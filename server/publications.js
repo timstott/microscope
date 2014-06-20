9acf489442e0898326ce8de008170b0141b0983c
Timothy Stott <stott.timothy@gmail.com>
2014-06-20 17:05:20 +0100

    PostsList pagination

--- server/publications.js
+++ server/publications.js
@@ -1,8 +1,12 @@
-Meteor.publish('posts', function () {
-  return Posts.find();
+Meteor.publish('posts', function (options) {
+  optionsAllowed = _.pick(options, 'limit', 'sort');
+  check(optionsAllowed, {limit: Match.Integer, sort: Match.Any});
+
+  return Posts.find({}, optionsAllowed);
 });
 
 Meteor.publish('comments', function (postId) {
+  check(postId, String);
   return Comments.find({postId: postId});
 });
 
